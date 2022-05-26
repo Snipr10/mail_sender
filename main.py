8de0c87978ace8d29cd297bfdf02075a6d1bb7ed
oleg.gusev <gusevoleg@gmail.com>
2022-05-26 12:59:48 +0300

    fix

--- main.py
+++ main.py
@@ -153,12 +153,12 @@ def sends():
 
 if __name__ == '__main__':
     SESSION = login(SESSION)
-    i, file_name = get_report(
-        "https://api.glassen-it.com/component/socparser/content/getReportDocxRef?period=day&thread_id=5284&reference_ids[]=1180&reference_ids[]=1184"
-    )
-    i.seek(0)
-    i = i.read()
-
-    send_message_email("gusevoleg96@gmail.com", i, file_name, "report_text")
+    # i, file_name = get_report(
+    #     "https://api.glassen-it.com/component/socparser/content/getReportDocxRef?period=day&thread_id=5284&reference_ids[]=1180&reference_ids[]=1184"
+    # )
+    # i.seek(0)
+    # i = i.read()
+    #
+    # send_message_email("gusevoleg96@gmail.com", i, file_name, "report_text")
 
     sends()
