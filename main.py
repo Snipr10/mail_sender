1d707a2e04694fbd2cbf35bd7638c2799626a267
oleg.gusev <gusevoleg@gmail.com>
2022-05-26 14:14:59 +0300

    fix

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ import json
 
 import threading
 import time
+from email import encoders
 from email.message import EmailMessage
 from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
 from io import BytesIO
 import datetime
 import smtplib, ssl
@@ -41,10 +43,10 @@ SESSION = login(requests.session())
 
 def get_report(uri):
     report = SESSION.get(uri)
-    print(report)
-    print(report.content)
-    print(uri)
+
     i = BytesIO(report.content)
+    open('test.docx', 'wb').write(report.content)
+
     file_name = bytes(
         report.headers.get('Content-Disposition').replace("attachment;filename=", "").replace(
             '"', ""), 'latin1').decode('utf-8')
@@ -61,17 +63,19 @@ def send_message_email(email_to, binary_data, file_name, report_text):
     msg['To'] = email_to
 
     file_name = "report.docx"
-    maintype, _, subtype = (mimetypes.guess_type(file_name)[0] or 'application/octet-stream').partition("/")
-    print(f"maintype {maintype}")
-    print(f"subtype {subtype}")
-    print(f"file_name {file_name}")
+    # maintype, _, subtype = (mimetypes.guess_type(file_name)[0] or 'application/octet-stream').partition("/")
+    # print(f"maintype {maintype}")
+    # print(f"subtype {subtype}")
+    # print(f"file_name {file_name}")
 
     msg.set_content('Добрый день! \n'
                     'В соответствии с выбранными вами временным интервалом и объектами мониторинга был'
                     f' сформирован отчёт по запросу по следующим субъектам/событиям: \n{report_text}'
                     )
-    msg.add_attachment(binary_data, maintype=maintype, subtype=subtype, filename="report.docx")
-
+    # msg.add_attachment(binary_data, maintype=maintype, subtype=subtype, filename="report.docx")
+    with open("test.docx", "rb") as fp:
+        msg.add_attachment(
+            fp.read(), maintype="file", subtype="doxc", filename="123.doxc")
 
     print("smtplib login")
 
@@ -153,12 +157,42 @@ def sends():
 
 if __name__ == '__main__':
     SESSION = login(SESSION)
-    # i, file_name = get_report(
-    #     "https://api.glassen-it.com/component/socparser/content/getReportDocxRef?period=day&thread_id=5284&reference_ids[]=1180&reference_ids[]=1184"
-    # )
-    # i.seek(0)
-    # i = i.read()
     #
-    # send_message_email("gusevoleg96@gmail.com", i, file_name, "report_text")
+    # msg = MIMEMultipart()
+    # fromaddr = 'SENDER EMAIL ADDRESS'
+    # toaddr = 'RECIPIENT EMAIL ADDRESS'
+    # # i, file_name = get_report(
+    # #     "https://api.glassen-it.com/component/socparser/content/getReportDocxRef?period=day&thread_id=5284&reference_ids[]=1180&reference_ids[]=1184"
+    # # )
+    # # i.seek(0)
+    # # i = i.read()
+    # # the_book = SESSION.get("https://api.glassen-it.com/component/socparser/content/getReportDocxRef?period=day&thread_id=5284&reference_ids[]=1180&reference_ids[]=1184", stream=True)
+    # # with open("1.docx", 'wb') as f:
+    # #   for chunk in the_book.iter_content(1024 * 1024 * 2):  # 2 MB chunks
+    # #     f.write(chunk)
+    # # send_message_email("gusevoleg96@gmail.com", i, file_name, "report_text")
+    # #
+    # port = 465
+    # context = ssl.create_default_context()
+    #
+    # msg = EmailMessage()
+    # msg['Subject'] = 'Автоматическая рассылка отчёта по выбранным субъектам/событиям'
+    # msg['From'] = EMAIL
+    # msg['To'] = "gusevoleg96@gmail.com"
+    #
+    # msg.set_content("Hello, victim!")
+    #
+    # with open("1.docx", "rb") as fp:
+    #     msg.add_attachment(
+    #         fp.read(), maintype="file", subtype="doxc", filename="aqw.doxc")
+    #
+    # with smtplib.SMTP_SSL("smtp.glassen-it.com", port, context=context) as server:
+    #     print("try login")
+    #
+    #     server.login(EMAIL_LOGIN, EMAIL_PASSWORD)
+    #     print("sucess login")
+    #     server.send_message(msg)
+    #     print("send")
+
 
     sends()
