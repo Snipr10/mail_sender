0e8da558fb3102bcdff302ef9260e9b4880d1384
oleg.gusev <gusevoleg@gmail.com>
2022-05-26 15:44:47 +0300

    fix

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ def send_message_email(email_to, i, file_name, report_text):
 
     msg.set_content('Добрый день! \n'
                     'В соответствии с выбранными вами временным интервалом и объектами мониторинга был'
-                    f' сформирован отчёт по запросу по следующим субъектам/событиям: \n{report_text}'
+                    f' сформирован отчёт по запросу по следующим субъектам/событиям: \n {report_text}'
                     )
 
     file_name = "report.docx"
@@ -101,7 +101,7 @@ def send_message_time(id_, uri, time_, email, report_text):
         if time_-seconds > 0:
             time.sleep(time_-seconds)
         try:
-            send_message_email("gusevoleg96@gmail.com", i, file_name, "report_text")
+            send_message_email("gusevoleg96@gmail.com", i, file_name, report_text)
             new, conn = get_cursor()
             new.execute(
                     "UPDATE `prsr_user_mail` SET is_prepare=0, last_mailing=? WHERE id=?", (datetime.datetime.now(), id_, )
