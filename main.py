82e13f2d474044ef33a2872636b72c08af724de5
oleg.gusev <gusevoleg@gmail.com>
2022-05-26 23:49:19 +0300

    fix

--- main.py
+++ main.py
@@ -15,10 +15,13 @@ from maria import get_cursor
 URL = "https://api.glassen-it.com/component/socparser/content/getReportDocxRef?period=%s&thread_id=%s"
 LOGIN_URL = "https://api.glassen-it.com/component/socparser/authorization/login"
 
-
 EMAIL = 'report@glassen-it.com'
 EMAIL_LOGIN = "report"
 EMAIL_PASSWORD = "J7sp7b8jf"
+
+
+def get_now():
+    return datetime.datetime.now() + datetime.timedelta(hours=3)
 
 
 def login(session):
@@ -31,7 +34,9 @@ def login(session):
         raise Exception("can not login")
     return session
 
+
 SESSION = login(requests.session())
+
 
 def get_report(uri):
     report = SESSION.get(uri)
@@ -72,24 +77,24 @@ def send_message_email(email_to, i, file_name, report_text):
 def send_message_time(id_, uri, time_, email, report_text):
     try:
         print(id_)
-        print(datetime.datetime.now() + datetime.timedelta(hours=3))
+        print()
 
         try:
             i, file_name = get_report(uri)
 
-            now_time = datetime.datetime.now() + datetime.timedelta(hours=3)
+            now_time = get_now()
             print(now_time)
-            seconds = now_time.second + now_time.minute*60 + now_time.hour*3600
-            print(time_-seconds)
-            if time_-seconds > 0:
-                time.sleep(time_-seconds)
+            seconds = now_time.second + now_time.minute * 60 + now_time.hour * 3600
+            print(time_ - seconds)
+            if time_ - seconds > 0:
+                time.sleep(time_ - seconds)
         except Exception as e:
             print(e)
         try:
             send_message_email(email.split(), i, file_name, report_text[1:-1])
             new, conn = get_cursor()
             new.execute(
-                "UPDATE `prsr_user_mail` SET is_prepare=0, last_mailing=? WHERE id=?", (datetime.datetime.now(), id_,)
+                "UPDATE `prsr_user_mail` SET is_prepare=0, last_mailing=? WHERE id=?", (get_now(), id_,)
             )
             conn.commit()
             conn.close()
@@ -106,15 +111,17 @@ def send_message_time(id_, uri, time_, email, report_text):
 
 
 def sends():
-    datetime.datetime.now()
+
     cur, set_conn = get_cursor()
     cur.execute(
         "SELECT id, last_mailing, mailing_time, reference_ids, thread_id, topics, email, period, is_prepare FROM "
         "`prsr_user_mail` WHERE is_prepare=0 and (`mailing_time` >= '23:50' or `mailing_time` >= ?)  and "
         "`last_mailing` < ?",
         (
-            (datetime.datetime.now() + datetime.timedelta(hours=3) - datetime.timedelta(minutes=10)).strftime('%H:%M:%S'),
-            (datetime.datetime.now() + datetime.timedelta(hours=3) - datetime.timedelta(hours=1)).strftime('%Y-%m-%d %H:%M:%S'),)
+            (get_now() - datetime.timedelta(minutes=10)).strftime(
+                '%H:%M:%S'),
+            (get_now() - datetime.timedelta(hours=1)).strftime(
+                '%Y-%m-%d %H:%M:%S'),)
     )
     new, conn = get_cursor()
 
@@ -141,4 +148,4 @@ if __name__ == '__main__':
             sends()
         except Exception as e:
             print(e)
-        time.sleep(1*60)
+        time.sleep(1 * 60)
