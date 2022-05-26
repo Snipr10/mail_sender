c5ed27b5447196d7f77db650198ec022cc40c6cb
oleg.gusev <gusevoleg@gmail.com>
2022-05-27 00:01:37 +0300

    fix

--- main.py
+++ main.py
@@ -115,7 +115,7 @@ def sends():
     cur, set_conn = get_cursor()
     cur.execute(
         "SELECT id, last_mailing, mailing_time, reference_ids, thread_id, topics, email, period, is_prepare FROM "
-        "`prsr_user_mail` WHERE is_prepare=0 and (`mailing_time` >= '23:50' or `mailing_time` >= ?)  and "
+        "`prsr_user_mail` WHERE is_prepare=0 and ('00:10' >= `mailing_time` or `mailing_time` >= '23:50' or `mailing_time` >= ?)  and "
         "`last_mailing` < ?",
         (
             (get_now() - datetime.timedelta(minutes=10)).strftime(
