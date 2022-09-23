108f0605bdb2dc6a6c8e9b8fa2aaee31355b638c
oleg.gusev <gusevoleg@gmail.com>
2022-09-23 22:24:21 +0300

    test send mail

--- main.py
+++ main.py
@@ -48,8 +48,9 @@ def get_report(uri, attempt=0):
     file_name = bytes(
         report.headers.get('Content-Disposition').replace("attachment;filename=", "").replace(
             '"', ""), 'latin1').decode('utf-8')
-    if attempt < 5 and\
-            report.content == b'"\xd0\xa7\xd1\x82\xd0\xbe-\xd1\x82\xd0\xbe \xd0\xbf\xd0\xbe\xd1\x88\xd0\xbb\xd0\xbe \xd0\xbd\xd0\xb5 \xd1\x82\xd0\xb0\xd0\xba"':
+    if attempt < 5 and \
+            (report.content == b'"\xd0\xa7\xd1\x82\xd0\xbe-\xd1\x82\xd0\xbe \xd0\xbf\xd0\xbe\xd1\x88\xd0\xbb\xd0\xbe \xd0\xbd\xd0\xb5 \xd1\x82\xd0\xb0\xd0\xba"'
+             or "Microsoft_Excel_Sheet" not in report.text):
         return get_report(uri, attempt+1)
     return i, file_name
 
